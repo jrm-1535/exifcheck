e046952c610e85127f51935485f7320ffca926d2
Jean-Rene Menand <jrmemand@yahoo.com>
2022-01-10 21:34:59 -0800

    added support for removal of individual tags and whole ifds: modified help text to describe the new -r option added -r= parsing with func parseRemoveString replaced magic numbers in getArgs with exported package constants modified processing of getThumbnailInfo to use the new package function GetIfdName replaced Format with FormatIfds added calls to RemoveTag and RemoveIfd

--- exifcheck.go
+++ exifcheck.go
@@ -4,6 +4,8 @@ package main
 import (
     "fmt"
     "flag"
+    "strings"
+    "strconv"
     "os"
     "io"
     "github.com/jrm-1535/exif"
@@ -21,7 +23,8 @@ const (
 
     HELP        = 
 `exifcheck [-h] [-v] [-w] [-u=keep|remove|stop] [-tiff] [-exif] [-maker] [-all]
-           [p=name] [-t] [-xo=name] [-xt=name] [-xmt=name] [-o=name] filepath
+           [p=name] [-t] [-xo=name] [-xt=name] [-xmt=name] 
+           [-r=id[:tag]*[,id[:tag]*]* [-o=name] filepath
 
     Check if a file has valid exif metadata, allowing to print metadata
     information about the picture and extracting various data from the file.
@@ -42,13 +45,27 @@ const (
         -all        print all metadata and maker notes
         -p=name     print metadata to a file (stdout by default)
 
-        -t          print thumbnail type, size and location in input file
+        -t          print thumbnail type, size and ifd location (may be more
+                    than one).
 
         -xo=name    extract original metadata into a new file
         -xt=name    extract exif thumbnail, if available, into a new file
         -xmt=name   extract maker thumbnail, if available, into a new file
 
-        -o=name     output processed metadata to a new file
+        -r=id[:tag]*[,id[:tag]*]*
+                    remove individual tag(s) specified by enclosing ifd id and
+                    tags in the ifd namespace, or whole ifdd. The syntax is a
+                    series of tuples id[:tag]* separated by commas. An id is
+                    is a decimal number whereas a tag can be specified as a
+                    decimal number or as a hex number (0xnn). An id alone
+                    (without a following :tag) means that the whole IFD must be
+                    removed. For example, -r=0:0x131:0x132,1 will remove tags
+                    0x131 (Software) and 0x132 (DateTime) from the primary IFD
+                    (IFD 0) and the whole thumbnail IFD (IFD 1) will be removed.
+
+        -o=name     output processed metadata (after possible tag and/or IFD
+                    removal) to a new file. Even if nothing was removed the
+                    layout may be different from the original metadata.
  
     filepath is the path to the file to process
 
@@ -64,9 +81,18 @@ const (
 `
 )
 
-type jpgArgs struct {
+type tagIds  struct {
+    ifdId                   exif.IfdId
+    tags                    []uint
+}
+
+type exifArgs struct {
     input, output, print    string
     original, tName, mtName string
+
+    removeIfds              []exif.IfdId
+    removeTags              []tagIds
+
     pTiff                   bool
     pExif                   bool
     pMaker                  bool
@@ -74,9 +100,59 @@ type jpgArgs struct {
     control                 exif.Control
 }
 
-func getArgs( ) (* jpgArgs, error ) {
+func parseRemoveString( pArgs *exifArgs, toRemove string ) error {
 
-    pArgs := new( jpgArgs )
+    // syntax ifdId[:tagId]*[,ifdId[:tagId]*]*
+    ifdIds := strings.Split( toRemove, "," )
+    for _, ifdId := range ifdIds {
+        // must start with an ifId, possibly followed by a series of :tagId
+        tags := strings.Split( ifdId, ":" )
+        ifid, err := strconv.ParseInt(tags[0], 0, 64);  if err != nil {
+            return fmt.Errorf( "syntax error: -r=%d\n", toRemove )
+        }
+
+        if len(tags) == 1 {
+            pArgs.removeIfds = append( pArgs.removeIfds, exif.IfdId(ifid) )
+        } else {
+            pArgs.removeTags = append( pArgs.removeTags,
+                                       tagIds{  exif.IfdId(ifid), []uint{} } )
+            for i := 1; i < len(tags); i++ {
+                tag, err := strconv.ParseUint(tags[i], 0, 64);  if err != nil {
+                    return fmt.Errorf( "syntax error: -r=%d\n", toRemove )
+                }
+                pArgs.removeTags[len(pArgs.removeTags)-1].tags = append(
+                                pArgs.removeTags[len(pArgs.removeTags)-1].tags,
+                                uint(tag) )
+            }
+        }
+    }
+    p := int(-1)
+    n := 0
+    for i := 0; i < len(pArgs.removeTags); i++ {
+        toKeep := true
+        for _, t := range pArgs.removeIfds {
+            if t == pArgs.removeTags[i].ifdId {
+                toKeep = false
+                if p == -1 { p = i }
+                break
+            }
+        }
+        if toKeep {
+            if p != -1 {
+                pArgs.removeTags[p] = pArgs.removeTags[i]
+                p ++
+            }
+            n ++
+        }
+
+    }
+    pArgs.removeTags = pArgs.removeTags[:n]
+    return nil
+}
+
+func getArgs( ) (* exifArgs, error ) {
+
+    pArgs := new( exifArgs )
 
     var version, all, warn, sDb, pDb bool
     flag.BoolVar( &version, "v", false, "print exifcheck version and exits" )
@@ -100,6 +176,9 @@ func getArgs( ) (* jpgArgs, error ) {
     flag.BoolVar( &sDb, "sd", false, "turn on debug printing during serialization" )
     flag.BoolVar( &pDb, "pd", false, "turn on debug printing during parsing" )
 
+    var toRemove string
+    flag.StringVar( &toRemove, "r", "", "Remove whole IFDs or individual tags in IFDs" )
+
     flag.Usage = func() {
         fmt.Fprintf( flag.CommandLine.Output(), HELP )
     }
@@ -119,10 +198,11 @@ func getArgs( ) (* jpgArgs, error ) {
 
     switch store[0] {
     case 'k', 'K': // nothing to change
+        pArgs.control.Unknown = exif.Keep
     case 'r', 'R':
-        pArgs.control.Unknown = 1
+        pArgs.control.Unknown = exif.Remove
     case 's', 'S':
-        pArgs.control.Unknown = 2
+        pArgs.control.Unknown = exif.Stop
     default:
         return nil, fmt.Errorf("Unknown action: %s\n", store )
     }
@@ -136,6 +216,10 @@ func getArgs( ) (* jpgArgs, error ) {
         pArgs.pMaker = true
     }
     pArgs.input = arguments[0]
+
+    if toRemove != "" {
+        parseRemoveString( pArgs, toRemove )
+    }
     return pArgs, nil
 }
 
@@ -156,8 +240,9 @@ func exifcheck() int {
     if process.pThumb {
         thbns := md.GetThumbnailInfo()
         for _, thbn := range thbns {
-            fmt.Printf( "%s type %s size %d\n", thbn.Origin,
-                        exif.GetCompressionString(thbn.Comp), thbn.Size )
+            fmt.Printf( "type %s size %d in %s IFD\n",
+                        exif.GetCompressionName(thbn.Comp),
+                        thbn.Size, exif.GetIfdName(thbn.Origin) )
         }
     }
 
@@ -192,7 +277,7 @@ func exifcheck() int {
                 return 1
             }
         }
-        md.Format( w, ifds )
+        md.FormatIfds( w, ifds )
     }
 
     if process.original != "" {
@@ -215,6 +300,25 @@ func exifcheck() int {
         _, err = md.WriteThumbnail( process.mtName, exif.EMBEDDED )
         if err != nil {
             fmt.Printf("Error writing %s: %v", process.mtName, err )
+            return 1
+        }
+    }
+
+    if len(process.removeTags) > 0 {
+//        fmt.Printf("Remove Tags: %v\n", process.removeTags )
+        for _, ti := range process.removeTags {
+            id := ti.ifdId
+            for _, tag := range ti.tags {
+                md.RemoveTag( id, tag )
+            }
+        }
+    }
+
+    for _, id := range process.removeIfds {
+        fmt.Printf("Remove ifd: %v\n", id )
+        err = md.RemoveIfd( id )
+        if err != nil {
+            fmt.Printf("Error removing ifd %s: %v", exif.GetIfdName(id), err )
             return 1
         }
     }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/jrm-1535/exifcheck
 
 go 1.17
 
-require github.com/jrm-1535/exif v0.0.0-20220107054833-9b44ce970e0e
+require github.com/jrm-1535/exif v0.0.0-20220111044430-10293f2b9dea
